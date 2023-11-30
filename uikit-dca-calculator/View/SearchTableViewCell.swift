5ec816f1015c140a2f36d1217e93ee7c44f4f21f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-11-30 23:59:21 +0900

    layout user interface and add prototype cell

---
+++ uikit-dca-calculator/View/SearchTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SearchTableViewCell.swift
+//  uikit-dca-calculator
+//
+//  Created by joe on 2023/11/30.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    @IBOutlet weak var assetNameLabel: UILabel!
+    @IBOutlet weak var assetSymbolLabel: UILabel!
+    @IBOutlet weak var assetTypeLabel: UILabel!
+}
