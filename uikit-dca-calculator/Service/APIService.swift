577884b7352e09c3f1efd74119c1ee15bf7d3e4e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-05 03:20:49 +0900

    percent encoding and combine error type

--- uikit-dca-calculator/Service/APIService.swift
+++ uikit-dca-calculator/Service/APIService.swift
@@ -9,6 +9,11 @@ import Foundation
 import Combine
 
 struct APIService {
+    enum APIServiceError: Error {
+        case encoding
+        case badRequest
+    }
+    
     var API_KEY: String {
         return keys.randomElement() ?? ""
     }
@@ -16,8 +21,10 @@ struct APIService {
     let keys = ["WS58F5EN3448FX1C", "HXMUNDJIX81H2UDJ", "6DUG1TOBABY1SOPG", "N483IC64XUG8M4VQ"]
     
     func fetchSymbolsPublisher(keywords: String) -> AnyPublisher<SearchResults, Error> {
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return Fail(error: APIServiceError.encoding).eraseToAnyPublisher() }
+        
         let urlString = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=\(keywords)&apikey=\(API_KEY)"
-        let url = URL(string: urlString)!
+        guard let url = URL(string: urlString) else { return Fail(error: APIServiceError.badRequest).eraseToAnyPublisher() }
         
         return URLSession.shared.dataTaskPublisher(for: url)
             .map({ $0.data })
