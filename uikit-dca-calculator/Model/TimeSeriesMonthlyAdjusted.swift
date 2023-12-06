1fb6ccdb3be03763bda1dabab01ecf47ffcc89b7
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-06 23:13:21 +0900

    create MonthInfo array using timeSeries

--- uikit-dca-calculator/Model/TimeSeriesMonthlyAdjusted.swift
+++ uikit-dca-calculator/Model/TimeSeriesMonthlyAdjusted.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+struct MonthInfo {
+    let date: Date
+    let adjustedOpen: Double
+    let adjustedClose: Double
+}
+
 struct TimeSeriesMonthlyAdjusted: Decodable {
     let meta: Meta
     let timeSeries: [String: OHLC]
@@ -14,6 +20,31 @@ struct TimeSeriesMonthlyAdjusted: Decodable {
     enum CodingKeys: String, CodingKey {
         case meta = "Meta Data"
         case timeSeries = "Monthly Adjusted Time Series"
+    }
+    
+    func getMonthInfos() -> [MonthInfo] {
+        var monthInfos: [MonthInfo] = []
+        let sortedTimeSeries = timeSeries.sorted(by: { $0.key > $1.key })
+        
+        sortedTimeSeries.forEach { dateString, ohlc in
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            
+            let date = dateFormatter.date(from: dateString) ?? Date()
+            let adjustedOpen = getAdjustedOpen(ohlc: ohlc)
+            let monthInfo = MonthInfo(date: date, adjustedOpen: adjustedOpen, adjustedClose: Double(ohlc.adjustedClose) ?? 0)
+            monthInfos.append(monthInfo)
+        }
+        
+        return monthInfos
+    }
+    
+    private func getAdjustedOpen(ohlc: OHLC) -> Double {
+        // adjusted open = open x (adjusted close / close)
+        let open = Double(ohlc.open) ?? 0
+        let adjustedClose = Double(ohlc.adjustedClose) ?? 0
+        let close = Double(ohlc.close) ?? 0
+        return open * (adjustedClose / close)
     }
 }
 
