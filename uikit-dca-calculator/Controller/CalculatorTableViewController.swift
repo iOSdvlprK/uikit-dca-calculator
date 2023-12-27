6199878ffb3fdff622d2a22af08cf264b5a62e69
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-12-27 23:27:09 +0900

    handle previous date selection with a checkmark

--- uikit-dca-calculator/Controller/CalculatorTableViewController.swift
+++ uikit-dca-calculator/Controller/CalculatorTableViewController.swift
@@ -18,6 +18,8 @@ class CalculatorTableViewController: UITableViewController {
     
     var asset: Asset?
     
+    private var initialDateOfInvestmentIndex: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -44,6 +46,7 @@ class CalculatorTableViewController: UITableViewController {
            let dateSelectionTableViewController = segue.destination as? DateSelectionTableViewController,
            let timeSeriesMonthlyAdjusted = sender as? TimeSeriesMonthlyAdjusted {
             dateSelectionTableViewController.timeSeriesMonthlyAdjusted = timeSeriesMonthlyAdjusted
+            dateSelectionTableViewController.selectedIndex = initialDateOfInvestmentIndex
             dateSelectionTableViewController.didSelectDate = { [weak self] index in
                 self?.handleDateSelection(at: index)
             }
@@ -54,6 +57,7 @@ class CalculatorTableViewController: UITableViewController {
         guard navigationController?.visibleViewController is DateSelectionTableViewController else { return }
         navigationController?.popViewController(animated: true)
         if let monthInfos = asset?.timeSeriesMonthlyAdjusted.getMonthInfos() {
+            initialDateOfInvestmentIndex = index
             let monthInfo = monthInfos[index]
             let dateString = monthInfo.date.MMYYFormat
             initialDateOfInvestmentTextField.text = dateString
