2e657b2e8ff139b46e29dcf8f3fe85728afeae00
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-01-07 22:29:56 +0900

    add first unit test

--- uikit-dca-calculatorTests/uikit_dca_calculatorTests.swift
+++ uikit-dca-calculatorTests/uikit_dca_calculatorTests.swift
@@ -21,17 +21,71 @@ final class uikit_dca_calculatorTests: XCTestCase {
         sut = nil
     }
     
+    // test cases
+    // 1. asset = winning | dca = true => positive gains
+    // 2. asset = winning | dca = false => positive gains
+    // 3. asset = losing | dca = true => negative gains
+    // 4. asset = losing | dca = false => negative gains
+    
     // Format for test function name
     // what
     // given
     // expectation
     
-    func testDCAResult_givenDCAIsUsed_expectResult() {
+    func testResult_givenWinningAssetAndDCAIsUsed_expectPositiveGains() {
+        // given
+        let initialInvestmentAmount: Double = 5000
+        let monthlyDollarCostAveragingAmount: Double = 1500
+        let initialDateOfInvestmentIndex: Int = 5
+        let asset = buildWinningAsset()
+        // when
+        let result = sut.calculate(
+            asset: asset,
+            initialInvestmentAmount: initialInvestmentAmount,
+            monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount,
+            initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
+        // then
+        // Initial investment: $5000
+        // DCA: $1500 x 5 = $7500
+        // total: $5000 + $7500 = $12500
+        XCTAssertEqual(result.investmentAmount, 12500, "investment amount is incorrect")
+    }
+    
+    func testResult_givenWinningAssetAndDCAIsNotUsed_expectPositiveGains() {
         
     }
     
-    func testDCAResult_givenDCAIsNotUsed_expectResult() {
+    func testResult_givenLosingAssetAndDCAIsUsed_expectNegativeGains() {
         
+    }
+    
+    func testResult_givenLosingAssetAndDCAIsNotUsed_expectNegativeGains() {
+        
+    }
+    
+    private func buildWinningAsset() -> Asset {
+        let searchResult = buildSearchResult()
+        let meta = buildMeta()
+        let timeSeries: [String: OHLC] = [
+            "2023-01-25": OHLC(open: "100", close: "0", adjustedClose: "110"),
+            "2023-02-25": OHLC(open: "110", close: "0", adjustedClose: "120"),
+            "2023-03-25": OHLC(open: "120", close: "0", adjustedClose: "130"),
+            "2023-04-25": OHLC(open: "130", close: "0", adjustedClose: "140"),
+            "2023-05-25": OHLC(open: "140", close: "0", adjustedClose: "150"),
+            "2023-06-25": OHLC(open: "150", close: "0", adjustedClose: "160")
+        ]
+        let timeSeriesMonthlyAdjusted = TimeSeriesMonthlyAdjusted(
+            meta: meta,
+            timeSeries: timeSeries)
+        return Asset(searchResult: searchResult, timeSeriesMonthlyAdjusted: timeSeriesMonthlyAdjusted)
+    }
+    
+    private func buildSearchResult() -> SearchResult {
+        return SearchResult(symbol: "XYZ", name: "XYZ Company", type: "ETF", currency: "USD")
+    }
+    
+    private func buildMeta() -> Meta {
+        return Meta(symbol: "XYZ")
     }
     
     func testInvestmentAmount_whenDCAIsUsed_expectResult() {
