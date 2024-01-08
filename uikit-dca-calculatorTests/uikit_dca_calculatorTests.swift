13cbc31a303230ab433e26ce73bd20dfdce36633
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-01-08 23:50:42 +0900

    complete first unit test

--- uikit-dca-calculatorTests/uikit_dca_calculatorTests.swift
+++ uikit-dca-calculatorTests/uikit_dca_calculatorTests.swift
@@ -49,6 +49,19 @@ final class uikit_dca_calculatorTests: XCTestCase {
         // DCA: $1500 x 5 = $7500
         // total: $5000 + $7500 = $12500
         XCTAssertEqual(result.investmentAmount, 12500, "investment amount is incorrect")
+        XCTAssertTrue(result.isProfitable)
+        
+        // Jan: $5000 / 100 = 50 shares
+        // Feb: $1500 / 110 = 13.6363 shares
+        // Mar: $1500 / 120 = 12.5 shares
+        // Apr: $1500 / 130 = 11.5384 shares
+        // May: $1500 / 140 = 10.7142 shares
+        // Jun: $1500 / 150 = 10 shares
+        // Total shares = 108.3889 shares
+        // Total current value = 108.3889 x 160 (latest month closing price) = $17,342.224
+        XCTAssertEqual(result.currentValue, 17342.224, accuracy: 0.1)
+        XCTAssertEqual(result.gain, 4842.224, accuracy: 0.1)
+        XCTAssertEqual(result.yield, 0.3873, accuracy: 0.0001)
     }
     
     func testResult_givenWinningAssetAndDCAIsNotUsed_expectPositiveGains() {
@@ -67,13 +80,21 @@ final class uikit_dca_calculatorTests: XCTestCase {
         let searchResult = buildSearchResult()
         let meta = buildMeta()
         let timeSeries: [String: OHLC] = [
-            "2023-01-25": OHLC(open: "100", close: "0", adjustedClose: "110"),
-            "2023-02-25": OHLC(open: "110", close: "0", adjustedClose: "120"),
-            "2023-03-25": OHLC(open: "120", close: "0", adjustedClose: "130"),
-            "2023-04-25": OHLC(open: "130", close: "0", adjustedClose: "140"),
-            "2023-05-25": OHLC(open: "140", close: "0", adjustedClose: "150"),
-            "2023-06-25": OHLC(open: "150", close: "0", adjustedClose: "160")
+            "2023-01-25": OHLC(open: "100", close: "110", adjustedClose: "110"),
+            "2023-02-25": OHLC(open: "110", close: "120", adjustedClose: "120"),
+            "2023-03-25": OHLC(open: "120", close: "130", adjustedClose: "130"),
+            "2023-04-25": OHLC(open: "130", close: "140", adjustedClose: "140"),
+            "2023-05-25": OHLC(open: "140", close: "150", adjustedClose: "150"),
+            "2023-06-25": OHLC(open: "150", close: "160", adjustedClose: "160")
         ]
+        // adjusted open = open x (adjusted close / close)
+        //               = 100 * 110 / 0 = 0 ⟶ close = 110
+        //               = 110 * 120 / 0 = 0 ⟶ close = 120
+        //               = 120 * 130 / 0 = 0 ⟶ close = 130
+        //               = 130 * 140 / 0 = 0 ⟶ close = 140
+        //               = 140 * 150 / 0 = 0 ⟶ close = 150
+        //               = 150 * 160 / 0 = 0 ⟶ close = 160
+        
         let timeSeriesMonthlyAdjusted = TimeSeriesMonthlyAdjusted(
             meta: meta,
             timeSeries: timeSeries)
